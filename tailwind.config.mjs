cfc99ba601f10d7978af3306fc50f9762867d94c
DanielRodado <d4nielrodado@gmail.com>
2024-08-02 17:51:30 -0500

    Proyect section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: "class",
+	safelist: [
+		{
+		  pattern: /bg-(red|green|blue|yellow|orange|purple|teal)-(200|300|400|500|600|700|800)/,
+		  variants: ['hover', 'focus', 'active'],
+		},
+	  ],
 	theme: {
 		extend: {},
 	},
