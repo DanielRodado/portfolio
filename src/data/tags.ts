be7ad09b8aaa4799cbc3112bdf7c922f10b6f80d
DanielRodado <d4nielrodado@gmail.com>
2024-08-03 15:26:42 -0500

    Se cambió el formato de proyects.json a proyects.ts, y se agregó un nuevo archivo a src/data/: 'tags.ts'

---
+++ src/data/tags.ts
@@ -0,0 +1,44 @@
+const tags = {
+    JAVA: {
+        name: "Java",
+        class: "bg-red-800 text-white",
+    },
+    SPRING: {
+        name: "Spring",
+        class: "bg-green-800 text-white",
+    },
+    POSTGRESQL: {
+        name: "PostgreSQL",
+        class: "bg-blue-500 text-white",
+    },
+    JAVASCRIPT: {
+        name: "Javascript",
+        class: "bg-yellow-500 text-black",
+    },
+    VUE: {
+        name: "Vue.js",
+        class: "bg-green-600 text-white",
+    },
+    HTML: {
+        name: "HTML",
+        class: "bg-orange-500 text-white",
+    },
+    CSS: {
+        name: "CSS",
+        class: "bg-blue-400 text-white",
+    },
+    BOOTSTRAP: {
+        name: "Bootstrap",
+        class: "bg-purple-900 text-white",
+    },
+    ASTRO: {
+        name: "Astro",
+        class: "bg-purple-600 text-white"
+    },
+    TAILWIND: {
+        name: "TailwindCSS",
+        class: "bg-blue-700/80 text-white"
+    }
+};
+
+export default tags;
