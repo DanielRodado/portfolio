a90eed934a9712d228e07d9664d069c6c81ce2df
DanielRodado <d4nielrodado@gmail.com>
2024-08-03 15:00:33 -0500

    Nuevas skills

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	safelist: [
 		{
-		  pattern: /bg-(red|green|blue|yellow|orange|purple|teal)-(200|300|400|500|600|700|800)/,
+		  pattern: /bg-(red|green|blue|yellow|orange|purple|teal)-(200|300|400|500|600|700|800|900)/,
 		  variants: ['hover', 'focus', 'active'],
 		},
 	  ],
